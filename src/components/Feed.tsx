303784580a7920031e5bc9f90e242908b7c809bb
Gustavo Holzmann <86106757+gusholz@users.noreply.github.com>
2022-08-03 12:47:33 -0300

    footer implementation

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -2,12 +2,22 @@ import './feed.css';
 
 export default function Feed(){
     return(
-        <div className='containerFeed' >
-            <img src="images/feedUpscale.png" alt="Feed" className='feed' />
-            <div>
-                <h1 className='news'>NEWS</h1>
-                <div className='parallelogram'></div>
+        <>
+            <div className='containerFeed' >
+                <h1 className='news'>News Feed</h1>
+                <div>
+                    <div className='parallelogram'></div>
+                    <div className='parallelogram2'></div>
+                </div>
             </div>
-        </div>
+            <div className='footer'>
+                <ul className='footerList'>
+                    <a className='footerLink' href='https://github.com/gusholz/countdownPersona'><li>Github</li></a>
+                    <a className='footerLink' href='https://www.linkedin.com/in/gustavo-holzmann-771781175/'><li>Linkedin</li></a>
+                    <a className='footerLink' href='https://www.behance.net/gusholz'><li>Behance</li></a>
+                    <a className='footerLink' href='https://github.com/gusholz/countdownPersona'><li>Paypal</li></a>
+                </ul>
+            </div>
+        </>
     )
 }
